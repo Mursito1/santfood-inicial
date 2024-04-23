329b3a57cfb670f46d7bfdcb1064dcbfa8fce0e9
Mursito1 <deltamur@hotmail.com>
2024-04-23 18:28:51 -0400

    arreglos en settings.py

--- myapp/santfood/settings.py
+++ myapp/santfood/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,18 +69,12 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'santfood.wsgi.app'
+WSGI_APPLICATION = 'santfood.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -117,6 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'santfood\static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'santfood\static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- myapp/santfood/urls.py
+++ myapp/santfood/urls.py
@@ -1,5 +1,7 @@
 from .views import inicio, nosotros, menus, registro
 from django.urls import path # type: ignore
+from django.conf.urls.static import static # type: ignore
+from django.conf import settings # type: ignore
 
 urlpatterns = [
     path('', inicio, name = 'inicio'),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('menus', menus, name = 'menus'),
     path('registro', registro , name = 'registro')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
